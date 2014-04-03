f724ba3579ce0c2a0782b21ff28045f25795804d
Backman <Arrebarree@gmail.com>
2014-04-03 12:00:54 +0200

    Third Person Character start

---
+++ Assets/Scripts/ThirdPersonCharacter.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThirdPersonCharacter : MonoBehaviour {
+	[Range(0.1f, 3.0f)] public float m_MoveSpeedMultiplier;
+	[Range(0.1f, 3.0f)] public float m_AnimationSpeedMultiplier;
+
+	Vector3 m_LookDirection;
+	Animator m_Animator;
+
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
