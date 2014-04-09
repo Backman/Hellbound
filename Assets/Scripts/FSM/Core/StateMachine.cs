6965815eda02478164750183a31f9b0a81ed77c2
Backman <Arrebarree@gmail.com>
2014-04-09 13:24:27 +0200

    State machine and pressure plate (Good, Bad) puzzle

---
+++ Assets/Scripts/FSM/Core/StateMachine.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public sealed class StateMachine <T> {
+	protected T m_Owner;
+
+	private State<T> m_CurrentState;
+	public State<T> CurrentState { get { return m_CurrentState; } }
+	private Dictionary<System.Type, State<T>> m_States = new Dictionary<System.Type, State<T>>();
+
+	public StateMachine(T owner, State<T> startState) {
+		m_Owner = owner;
+
+		addState(startState);
+		m_CurrentState = startState;
+	}
+
+	public void addState(State<T> state) {
+		state.setMachineAndOwner(this, m_Owner);
+		m_States[state.GetType()] = state;
+	}
+
+	public void update(float deltaTime) {
+		m_CurrentState.reason(m_Owner);
+		m_CurrentState.execute(m_Owner, deltaTime);
+	}
+
+	public S changeState<S>() where S : State<T> {
+		var newType = typeof(S);
+		if(m_CurrentState.GetType() == newType) {
+			return m_CurrentState as S;
+		}
+
+		if(m_CurrentState != null) {
+			m_CurrentState.exit(m_Owner);
+		}
+
+#if UNITY_EDITOR
+		if(!m_States.ContainsKey(newType)) {
+			var error = "ERROR: State " + newType + " does not exist in the state list..."+
+				"Have you added it by calling addState?";
+			Debug.LogError(error);
+			throw new UnityException(error);
+		}
+#endif
+		m_CurrentState = m_States[newType];
+		m_CurrentState.enter(m_Owner);
+
+		return m_CurrentState as S;
+	}
+}
---
+++ Assets/Scripts/FSM/States/GoodState.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GoodState : State<PressurePlate> {
+
+	public override void enter (PressurePlate entity)
+	{
+		entity.gameObject.renderer.material.color = Color.green;
+		/*foreach(PressurePlate obj in entity.m_ObjectsToAffect){
+			if(obj.m_Symbol == PressurePlate.Symbol.Fire) {
+				obj.m_Symbol = PressurePlate.Symbol.Wind;
+				obj.gameObject.renderer.material.color = Color.green;
+			} else {
+				Debug.Log (obj.name + " is changing state to BadState");
+				obj.changeState<BadState>();
+				obj.m_Symbol = PressurePlate.Symbol.Fire;
+				obj.gameObject.renderer.material.color = Color.cyan;
+			}
+		}*/
+		//Debug.Log("Entering Goodstate!");
+	}
+
+	public override void reason (PressurePlate entity) {	
+		/*if(entity.m_Symbol == PressurePlate.Symbol.Earth) {
+			m_Machine.changeState<BadState>();
+		}*/
+	}
+
+	public override void execute (PressurePlate entity, float deltaTime) {
+		//Debug.Log ("Is in Goodstate!");
+	}
+
+	public override void exit (PressurePlate entity) {
+		//Debug.Log("Leaving Goodstate...");
+	}
+
+	public override void activate (PressurePlate entity) {
+		Messenger.Broadcast("plate triggered", entity.m_Symbol);
+		Debug.Log ("You're good mate!");
+	}
+}
