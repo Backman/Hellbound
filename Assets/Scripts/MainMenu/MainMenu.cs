67517600fa14f840f6640efa32d4f3661c524de2
Jimmy <nintendojimmy@hotmail.com>
2014-04-11 16:18:38 +0200

    MainMenu

---
+++ Assets/Scripts/MainMenu/MainMenu.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MainMenu : MonoBehaviour {
+
+	void OnClickButton(){
+		Debug.Log ("Clicked on button!");
+		Application.LoadLevel("Antons_Scene");
+	}
+
+	void OnClickOptionsButton(){
+		Debug.Log ("Clicked on Options button");
+	}
+
+	void OnClickExitButton(){
+		Debug.Log ("Clicked on Exit button");
+	}
+}
