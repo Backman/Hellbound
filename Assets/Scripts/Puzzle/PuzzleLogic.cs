8da648f3215f20e94bc279f65f1502282ad4e818
Aleksi <aleksi_888@hotmail.com>
2014-04-14 16:32:05 +0200

    Pause/Inventory

---
+++ Assets/Scripts/Puzzle/PuzzleLogic.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public abstract class PuzzleLogic : MonoBehaviour {
+	[SerializeField] string m_EventToHandle;
+	// Use this for initialization
+	void Start () {
+		Messenger.AddListener<GameObject>(m_EventToHandle, onEvent);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public abstract void onEvent(GameObject obj);
+}
