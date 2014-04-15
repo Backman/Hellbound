d253c781c3f82b37675dde8bdea9b86f9ae4d7e8
Aleksi <aleksi_888@hotmail.com>
2014-04-15 14:34:32 +0200

    New in-game inventory GUI

--- Assets/Scripts/Inventory/InventoryMouseEvent.cs
+++ Assets/Scripts/Inventory/InventoryMouseEvent.cs
@@ -4,12 +4,11 @@ using System.Collections.Generic;
 
 public class InventoryMouseEvent : MonoBehaviour {
 
-	private static bool m_LeftMouseDown, m_RightMouseDown;
+	private static bool m_LeftMouseDown;
 	private static GameObject m_MouseDownObject;
 	// Use this for initialization
 	void Start () {
 		m_LeftMouseDown = Input.GetKeyDown(KeyCode.Mouse0);
-		m_RightMouseDown = Input.GetKeyDown(KeyCode.Mouse1);
 	}
 	
 	// Update is called once per frame
@@ -22,62 +21,25 @@ public class InventoryMouseEvent : MonoBehaviour {
 			m_LeftMouseDown = false;
 			onMouseRelease(0);
 		}
-
-		if(Input.GetKeyDown(KeyCode.Mouse1) && !m_RightMouseDown){
-			m_RightMouseDown = true;
-			onMouseClick(1);
-		}
-		else if(Input.GetKeyUp(KeyCode.Mouse1) && m_RightMouseDown){
-			m_RightMouseDown = false;
-			onMouseRelease(1);
-		}
 	}
 
 	// Button 0 is mouse0, (Left mouse button)
 	// Button 1 is mouse1, (Right mouse button)
 	void onMouseClick(int button){
-		if(button == 1) {
+		if(button == 0) {
 			m_MouseDownObject = UICamera.hoveredObject;
-		}
-		
-		bool hide = true;
-		if(UICamera.hoveredObject != null){
-			UISprite sprite = UICamera.hoveredObject.GetComponent<UISprite>();
-			if(sprite != null){
-				if(sprite.name == "ExamineWindow"){
-					hide = false;
-				}
-				UIRect child = sprite.parent;
-				// Check if the sprite or its parents name is ExamineWindow
-				// and only hide examine window when pressing outside it
-				if(hide){
-					while(child != null){
-						if(child.name == "ExamineWindow"){
-							hide = false;
-							break;
-						}
-						child = child.parent;
-					}
-				}
-				if(hide){
-					Inventory.getInstance().hideInventoryMenu();
-				}
-			}
-			else{
-				Inventory.getInstance().hideInventoryMenu();
-			}
-		}
-		else{
-			Inventory.getInstance().hideInventoryMenu();
 		}
 	}
 
 	void onMouseRelease(int button){
-		if(m_MouseDownObject && button == 1) {
-			InventoryItem invItem = m_MouseDownObject.GetComponent<InventoryItem> ();
-			if (invItem != null) {
+		if(m_MouseDownObject && button == 0) {
+			InventoryItem invItem = m_MouseDownObject.GetComponent<InventoryItem>();
+			if (UICamera.hoveredObject == m_MouseDownObject && invItem != null) {
+				PreviewItems.getInstance().previewItem(invItem.name);
+				Debug.Log("Preview: "+invItem.name);
 				Inventory.getInstance().setSelectedItem(invItem);
 				Inventory.getInstance().showInventoryMenu();
+				Inventory.getInstance().addCombineItem(invItem, false);
 			}
 			m_MouseDownObject = null;
 		}
---
+++ Assets/Scripts/Inventory/PreviewOil.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PreviewOil : PreviewItem {
+	protected override void Awake(){
+		PreviewItems.getInstance().add("Oil", this);
+		gameObject.SetActive(false);
+	}
+	
+	public override void show(){
+		gameObject.SetActive(true);
+	}
+	
+	public override void hide(){
+		gameObject.SetActive(false);
+	}
+}
