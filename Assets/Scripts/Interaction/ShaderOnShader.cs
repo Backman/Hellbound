d099cc8dbee1c48174930736091d01fdaf3927eb
Peter <peter.nilsson.1991@hotmail.com>
2014-04-15 10:23:10 +0200

    outline

---
+++ Assets/Scripts/Interaction/ShaderOnShader.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+//By Peter Nilsson 04-14-14
+
+[RequireComponent(typeof(Camera))]
+public class ShaderOnShader : MonoBehaviour {
+	public Shader shady;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
