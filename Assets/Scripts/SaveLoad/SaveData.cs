41fe38c101e79d5e2345e44d1e9743cff97182db
Aleksi <aleksi_888@hotmail.com>
2014-04-16 17:23:47 +0200

    update

---
+++ Assets/Scripts/SaveLoad/SaveData.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using System;
+using System.IO;
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Formatters.Binary;
+
+[Serializable()]
+public class SaveData : ISerializable{
+	public bool foundGem;
+	public int checkpoint;
+	public SaveData(){
+		foundGem = true;
+		checkpoint = 23;
+	}
+	
+	/// <summary>
+	/// Deserialization constructor.
+	/// </summary>
+	public SaveData(SerializationInfo info, StreamingContext context){
+		foundGem = (bool)info.GetValue("foundGem", typeof(bool));
+		checkpoint = (int)info.GetValue("checkpoint", typeof(int));
+	}
+	
+	/// <summary>
+	/// Serialization function.
+	/// </summary>
+	public void GetObjectData(SerializationInfo info, StreamingContext context){
+		info.AddValue("foundGem", foundGem);
+		info.AddValue("checkpoint", checkpoint);
+	}
+}
