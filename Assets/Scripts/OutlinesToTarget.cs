de4cdacb7f50f9373698829e5d9bdcb7f16c58e2
Peter <peter.nilsson.1991@hotmail.com>
2014-04-21 11:00:04 +0200

    today

---
+++ Assets/Scripts/OutlinesToTarget.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class OutlinesToTarget : MonoBehaviour {
+	public GameObject target;
+	public Shader shader;
+	// Use this for initialization
+	void Start () {
+		GameObject temp = Instantiate(target) as GameObject;
+		temp.renderer.material.shader = shader;
+
+
+		target = temp;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
