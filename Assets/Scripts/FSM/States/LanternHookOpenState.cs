145965e0922058e43418699fde68876cbf33d876
Simon Jonasson <ksr.jonasson@gmail.com>
2014-04-24 16:36:13 +0200

    yea

--- Assets/Scripts/FSM/States/LanternHookOpenState.cs
+++ Assets/Scripts/FSM/States/LanternHookOpenState.cs
@@ -3,11 +3,14 @@ using System.Collections;
 
 [System.Serializable]
 public class LanternHookOpenState : State<LanternHook> {
-
+	public GameObject[] m_ObjectsToActivate; 
 
 	public override void enter (LanternHook entity){
 		Debug.Log ("LanternHook is Open");
 		entity.m_ObjectToOpen.useWith(entity.gameObject);
+		foreach( GameObject g in m_ObjectsToActivate ){
+			g.SetActive(true);
+		}
 	}
 }
 
