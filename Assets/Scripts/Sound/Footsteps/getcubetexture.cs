ec718c8a0550558d88a9798d18a84c2e9d3576a6
Scunnder <anton.thorsell@hotmail.com>
2014-04-28 16:49:21 +0200

    sure

--- Assets/Scripts/Sound/Footsteps/getcubetexture.cs
+++ Assets/Scripts/Sound/Footsteps/getcubetexture.cs
@@ -4,21 +4,14 @@ using System.Collections;
 public class getcubetexture : MonoBehaviour {
 
 
+
+	private Transform m_ObjectUnderThis;
+
 	void Start () {
 
-
-		/*
-		Renderer renderer = hit.collider.renderer;
-		MeshCollider meshCollider = hit.collider as MeshCollider;
-
-		Texture2D tex = renderer.material.mainTexture as Texture2D;
-
-		tex.GetNativeTextureID ();
-
-		Vector2 pixelUV = hit.textureCoord;
-		pixelUV.x *= tex.width;
-		pixelUV.y *= tex.height;
-		*/
+	//TODO: test if making a list works well
+	// or if i should make everything into numbers
+	//(ask anton)
 	}
 
 	void Update () {
@@ -28,14 +21,18 @@ public class getcubetexture : MonoBehaviour {
 
 		if(Physics.Raycast(theRay, out hit)){
 			if(hit.collider.GetComponent<FootstepSurface>() != null){
-
-				Vector2 texCoord = hit.textureCoord;
-				Texture textureHit = hit.transform.renderer.material.GetTexture("_MainTex");
-				Debug.Log(textureHit.name + " :name of texture");
-				Debug.Log (hit.transform.name + " :name of transform");
-
+				m_ObjectUnderThis = hit.transform;
 			}
 			
 		}
+	}
+
+	public int getStrongestSurface(){
+		int ret = 0;
+
+		Texture maintex = m_ObjectUnderThis.renderer.material.mainTexture as Texture;
+
+
+		return ret;
 	}
 }
