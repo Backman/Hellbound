dcb63ca9473b8b4386531a7f9b1ce89a041d0462
Peter <peter.nilsson.1991@hotmail.com>
2014-04-29 18:41:40 +0200

    update

--- Assets/Scripts/HUD/Menu.cs
+++ Assets/Scripts/HUD/Menu.cs
@@ -4,10 +4,11 @@ using System.Collections;
 //generel menu script för alla menyer som gör det flexiblare att skapa nya menyer
 public class Menu : MonoBehaviour {
 	private GameObject r_SettingsMenu;
+	public bool Active = false;
 	// Use this for initialization
 	void Awake () {
 		r_SettingsMenu = gameObject;
-		gameObject.SetActive(false);
+		gameObject.SetActive(Active);
 	}
 	
 	// Update is called once per frame
@@ -16,7 +17,10 @@ public class Menu : MonoBehaviour {
 	}
 
 	public void show(){
-		StartMenu.setVisible(false);
 		r_SettingsMenu.SetActive (true);
+	}
+
+	public void dontShow(){
+		r_SettingsMenu.SetActive (false);
 	}
 }
