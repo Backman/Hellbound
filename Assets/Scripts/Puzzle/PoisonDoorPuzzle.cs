af2118beff31ab4001bcbedb08315cfb8443d913
Backman <Arrebarree@gmail.com>
2014-05-02 10:16:34 +0200

    Puzzle prefabs

--- Assets/Scripts/Puzzle/PoisonDoorPuzzle.cs
+++ Assets/Scripts/Puzzle/PoisonDoorPuzzle.cs
@@ -6,11 +6,17 @@ public class PoisonDoorPuzzle : MonoBehaviour {
 	// Use this for initialization
 	void Start () {
 		Messenger.AddListener<GameObject, bool>("onDoorPoisonDrunk", onDoorPoisonDrunk);
+		Messenger.AddListener<GameObject, bool>("onDrinkDoorPoison", onDrinkDoorPoison);
 	}
 	
 	// Update is called once per frame
 	void Update () {
 	
+	}
+
+	public void onDrinkDoorPoison(GameObject obj, bool tr) {
+		obj.SetActive (false);
+		Messenger.Broadcast("clear focus");
 	}
 	
 	public void onDoorPoisonDrunk(GameObject thisObject, bool triggerOnlyForThis){
