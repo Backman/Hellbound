623bc2275d9f0c3501a5a451256323963226f014
Isabella <isabella.billgren@gmail.com>
2014-05-02 16:49:13 +0200

    War TEST READY!!!

--- Assets/Scripts/FSM/States/PressurePlate.cs
+++ Assets/Scripts/FSM/States/PressurePlate.cs
@@ -30,11 +30,7 @@ public class PressurePlate : Interactable {
 	}
 
 	void OnTriggerEnter(Collider col) {
-		if(m_ActivateType == ActivateType.OnTrigger){
-			if(col.tag == "Player") {
-				activate();
-			}
-		}
+		activate();
 		PuzzleEvent.trigger("onTriggerEnter", gameObject, true);
 	}
 
@@ -43,7 +39,7 @@ public class PressurePlate : Interactable {
 	}*/
 
 	public override void activate () {
-		m_Machine.CurrentState.activate(this);
+		base.activate ();
 	}
 
 	public S changeState<S>() where S : State<PressurePlate> {
