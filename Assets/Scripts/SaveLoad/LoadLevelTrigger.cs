12344914f9fc763d049b368a2eaca640d12bd9e9
Simon Jonasson <ksr.jonasson@gmail.com>
2014-05-06 14:01:51 +0200

    Tried ignoring lux

---
+++ Assets/Scripts/SaveLoad/LoadLevelTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/// <summary>
+/// This scirpt is attached to a trigger object and when the player
+/// enters the trigger zone, this script will trigger a level load.
+/// 
+/// Created by Simon
+/// </summary>
+public class LoadLevelTrigger : MonoBehaviour {
+	[SerializeField]
+	private string m_LevelToLoad;
+
+	[Multiline] [SerializeField]
+	private string m_LoadMessage;
+
+	void OnTriggerEnter( Collider col ){
+		if( col.tag == "Player" ){
+			LoadingLogic.Instance.loadLevel( m_LevelToLoad, m_LoadMessage );
+			gameObject.SetActive(false);
+		}
+	}
+}
