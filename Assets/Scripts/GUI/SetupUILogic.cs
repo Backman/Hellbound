0cae7371e85b42cc6aca12c799d79392798643a8
Backman <Arrebarree@gmail.com>
2014-05-07 12:36:59 +0200

    Loading scenes...

--- Assets/Scripts/GUI/SetupUILogic.cs
+++ Assets/Scripts/GUI/SetupUILogic.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class SetupUILogic : MonoBehaviour {
 	public GameObject m_UiToInstantiate;
 
-	void Start(){
+	void Awake(){
 		if( FindObjectOfType( typeof(UIRoot) ) == null ){
 			Instantiate( m_UiToInstantiate, Vector3.zero + Vector3.up*-100.0f, Quaternion.identity);
 		}
