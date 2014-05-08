c5d535e3f7a3fb80d2553cc4c8206a78a3752e65
Scunnder <anton.thorsell@hotmail.com>
2014-05-08 15:59:00 +0200

    tipsbutton + handler and a prefab

---
+++ Assets/Scripts/GUI/TipsButton.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipsButton : MonoBehaviour {
+
+
+
+	// Use this for initialization
+	void Start () {
+		transform.GetChild (0).gameObject.SetActive(false);
+	}
+
+
+	public void OnClick(){
+		transform.GetChild (0).gameObject.SetActive(true);
+	}
+
+	public void OnDisable(){
+		transform.GetChild (0).gameObject.SetActive(false);
+	}
+}
---
+++ Assets/Scripts/GUI/TipsHandler.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TipsHandler : MonoBehaviour {
+
+	[System.Serializable]
+	public class Tips{
+		[SerializeField][Multiline]
+		public string Text;
+	}
+	[HideInInspector]
+	public GameObject r_Prefab;
+	public Tips[] tips;
+
+	void Start(){
+		GameObject tipsPanel = GameObject.FindGameObjectWithTag ("TipsPanel");
+
+		int Finished_Buttons = 0;
+
+		foreach (Tips t in tips) {
+
+			GameObject newTips = (GameObject)Instantiate(r_Prefab);
+
+			newTips.transform.parent = GameObject.FindGameObjectWithTag("TipsPanel").transform;
+
+			newTips.name = "TipsButton";
+			newTips.GetComponentInChildren<UILabel>().text = t.Text;
+			Vector3 newValue = new Vector3(0f,(float)(Finished_Buttons * 525), 0);
+			newTips.transform.position = newValue;
+
+			Finished_Buttons++;
+		}
+
+	}
+}
