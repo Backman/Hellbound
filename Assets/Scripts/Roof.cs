2ded168ee47c8f8e5260a33ffecdb70bfca03e8b
Simon Jonasson <ksr.jonasson@gmail.com>
2014-05-12 16:09:59 +0200

    Fixes the bugs in pressure plate puzzle

--- Assets/Scripts/Roof.cs
+++ Assets/Scripts/Roof.cs
@@ -2,15 +2,46 @@
 using System.Collections;
 
 public class Roof : MonoBehaviour {
+	[SerializeField][Tooltip("The tween-player for lowering the roof")]
+	private UIPlayTween m_Tweener;
 
-	public float m_LowerAmount = 1.0f;
+	private enum Phase {Start, InMotion, Wait, Kill};
+	Phase m_Phase = Phase.Start;
 
 	// Use this for initialization
 	void Start () {
+		Messenger.AddListener ("start roof", startRoof );
 		Messenger.AddListener ("lower roof", lowerRoof );
 	}
 
+	
+
+	public void startRoof(){
+		if( m_Phase == Phase.Start ){
+			Debug.Log("Starting roof");
+			m_Tweener.Play(true);
+			m_Phase = Phase.InMotion;
+		} else {
+			Debug.Log("Error! Invalid tween state of the roof");
+		}
+	}
+
+	public void startDone(){
+		if( m_Phase == Phase.InMotion ){
+			m_Tweener.tweenGroup = 1;
+			m_Phase = Phase.Wait;
+		} else {
+			Debug.Log("Error! Invalid tween state of the roof");
+		}
+	}
+
 	public void lowerRoof(){
-		gameObject.transform.Translate (new Vector3 (0.0f, -m_LowerAmount, 0.0f));
+		if( m_Phase == Phase.Wait ) {
+			Debug.Log("Killing player");
+			m_Tweener.Play(true);
+			m_Phase = Phase.InMotion;
+		} else {
+			Debug.Log("Error! Invalid tween state of the roof");
+		}
 	}
 }
