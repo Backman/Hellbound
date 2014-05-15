32cf6c52210fdac8c8f59beeca5be0f21949f8fc
Gikkman <ksr.jonasson@gmail.com>
2014-05-15 14:47:02 +0200

    merge

---
+++ Assets/Scripts/Camera/BlurEffect.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlurEffect : MonoBehaviour {
+
+	[TooltipAttribute("The amount of blur imposed on the screen when this script is active")]
+	public float m_BlurAmount = 0.5f;
+	public Shader m_BlurShader = null;
+
+
+}
