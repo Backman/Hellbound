d2595638aac3130317ba6e00382b9076a54beae0
Gikkman <ksr.jonasson@gmail.com>
2014-05-20 16:00:46 +0200

    More response on whiskey

--- Assets/Scripts/Puzzle/PoisonDoorPuzzle.cs
+++ Assets/Scripts/Puzzle/PoisonDoorPuzzle.cs
@@ -5,6 +5,7 @@ public class PoisonDoorPuzzle : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
+		Messenger.AddListener<GameObject, bool> ("onBottleRemove", onBottleRemove);
 		Messenger.AddListener<GameObject, bool>("onMoveBottleToHand", onMoveBottleToHand);
 		Messenger.AddListener<GameObject, bool>("onDoorPoisonDrunk", onDoorPoisonDrunk);
 		Messenger.AddListener<GameObject, bool>("onDrinkDoorPoison", onDrinkDoorPoison);
@@ -14,9 +15,12 @@ public class PoisonDoorPuzzle : MonoBehaviour {
 	void Update () {
 	
 	}
-	public void onMoveBottleToHand(GameObject obj, bool tr) {
-		Interactable inter = obj.GetComponent<Interactable> ();
 
+	//Called at the beginning of the "drink" animation
+	public void onMoveBottleToHand(GameObject obj, bool tr) {
+		Messenger.Broadcast ("clear focus");
+
+		Interactable inter = obj.GetComponent<Interactable> ();
 		inter.setPuzzleState("inUse");
 
 		//Move the bottle to the players hand
@@ -26,29 +30,31 @@ public class PoisonDoorPuzzle : MonoBehaviour {
 		obj.transform.localPosition = Vector3.zero;
 
 	}
-
+	//Called at the peak of the "drink" animation
 	public void onDrinkDoorPoison(GameObject obj, bool tr) {
-
-		Messenger.Broadcast("clear focus");
-		GameObject.Destroy (obj);
-
+		Interactable inter = obj.GetComponent<Interactable> ();
+		inter.setPuzzleState ("used");
 	}
-	
+
+	//Called at the end of the "drink" animation
+	public void onBottleRemove(GameObject obj, bool tr){
+		GameObject.Destroy (obj);
+	}
+
+	//Called at the end of the "drink" animation
 	public void onDoorPoisonDrunk(GameObject thisObject, bool triggerOnlyForThis){
 		StartCoroutine("startSickness", gameObject );
 	}
 
+	//Will gradually apply sickness effect to the camera
 	IEnumerator startSickness(GameObject doorToOpen) {
-		yield return new WaitForSeconds (1.0f);
-		gameObject.GetComponent<Behaviour_DoorSimple>().unlockAndOpen();
-
 		float t = 0.0f;
+
 		MotionBlur motionBlur = Camera.main.GetComponent<MotionBlur>();
 		motionBlur.enabled = true;
 		 
 		CameraBlur blur = Camera.main.GetComponent<CameraBlur> ();
 		blur.enabled = true;
-
 		while(t < 1.0f) {
 			motionBlur.blurAmount = t * 0.8f;
 			blur.setBlurPercentage(t);
@@ -59,5 +65,6 @@ public class PoisonDoorPuzzle : MonoBehaviour {
 		}
 		motionBlur.blurAmount = 0.8f;
 		blur.setBlurPercentage (1.0f);
+		gameObject.GetComponent<Behaviour_DoorSimple>().unlockAndOpen();
 	}
 }
