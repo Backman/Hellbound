ab96c353aeefb548a8896f4c4a5ebd940e1c4171
Gikkman <ksr.jonasson@gmail.com>
2014-05-28 16:39:06 +0200

    Spawn now only activates a disabled object, rather than instantiating a prefab (which is slow)

--- Assets/Scripts/SpawnObject.cs
+++ Assets/Scripts/SpawnObject.cs
@@ -3,20 +3,27 @@ using System.Collections;
 
 /// <summary>
 /// Spawns a specific GameObject at a position
-/// when a GameObject with a specific tag enters it's collision box
+/// when a GameObject with a specific tag enters it's collision box.
 /// By Arvid Backman
 /// </summary>
 
 public class SpawnObject : MonoBehaviour {
+	[TooltipAttribute("The object that should be spawned. Note that this should be an object in the world, NOT a prefab.\n" +
+					  "The object will be disabled upon startup and enabled when this zone is enterd")]
 	public GameObject m_SpawnObject;
+	[TooltipAttribute("If this variable is set, the object will be moved to this position when the trigger zone is entered")]
 	public Transform m_Position;
 	public string m_ColliderTag = "Player";
 	public float m_Delay = 0.0f;
+
 	private bool m_Triggered = false;
 
-	void Start() {
-		if(m_Position == null){
-			m_Position = transform;
+	void Awake() {
+		if(m_SpawnObject == null){
+			Debug.LogWarning("Warning. No GameObject assigned in " + gameObject.name +". Disabling self");
+			gameObject.SetActive(false);
+		} else {
+			m_SpawnObject.SetActive (false);
 		}
 	}
 
@@ -31,6 +38,11 @@ public class SpawnObject : MonoBehaviour {
 
 	IEnumerator spawnObject() {
 		yield return new WaitForSeconds(m_Delay);
-		GameObject obj = (GameObject)Instantiate(m_SpawnObject, m_Position.position, m_Position.rotation);
+
+		if( m_SpawnObject != null )
+			m_SpawnObject.SetActive( true );
+
+		if( m_Position != null )
+			m_SpawnObject.transform.position = m_Position.transform.position;
 	}
 }
