36f5ed05dcf3c5e6070f9955402f065b32f2afed
Scunnder <anton.thorsell@hotmail.com>
2014-05-28 13:19:22 +0200

    fuck

--- Assets/Scripts/GUI/GUIManager.cs
+++ Assets/Scripts/GUI/GUIManager.cs
@@ -208,7 +208,7 @@ public class GUIManager : Singleton<GUIManager> {
 	/// <param name="text">Text.</param>
 	/// <param name="button">Button.</param>
 	/// <param name="lockMovement">If set to <c>true</c> lock movement.</param>
-	public void simpleShowText(string text, string button = "Examine", bool lockMovement = true){
+	public bool simpleShowText(string text, string button = "Examine", bool lockMovement = true){
 		if( !m_Examining ){
 			m_Examining = true;
 			object[] args = new object[5];
@@ -219,9 +219,13 @@ public class GUIManager : Singleton<GUIManager> {
 			args[4] = false;
 			
 			StartCoroutine("examine", args);
+			Debug.Log("tuuue");
+			return true;
 		}
 		else {
 			Debug.Log("Bussy examining");
+			Debug.Log("alllse");
+			return false;
 		}
 	}
 	
--- Assets/Scripts/Sound/AlterSounds/AlterSoundParameter.cs
+++ Assets/Scripts/Sound/AlterSounds/AlterSoundParameter.cs
@@ -193,4 +193,9 @@ public class AlterSoundParameter : MonoBehaviour {
 
 		return returnThis;
 	}
+
+	void derp()
+	{
+
+	}
 }
