1ce369f508df543b48761c1626f6bd168af38430
Peter <peter.nilsson.1991@hotmail.com>
2014-05-29 15:18:32 +0200

    font

---
+++ Assets/Editor/ShadowOptionEditor.cs
@@ -0,0 +1,29 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+
+/// <summary>
+/// Class to draw inspector layout for Checkpoint
+/// </summary>
+[CustomEditor(typeof(EditorNone))]
+public class ShadowOptionEditor : Editor {
+
+	public void OnEnable(){
+		hideFlags = HideFlags.HideAndDontSave;
+	}
+	/*
+	public override void OnInspectorGUI(){
+		ShadowOption shadowOption = target as ShadowOption;
+		List<string> bla = shadowOption.gameObject.GetComponent<UIPopupList>().items;
+		NGUIEditorTools.SetLabelWidth(80f);
+		foreach (string name in bla) {
+			//float temp = GUI.skin.label.CalcSize(new GUIContent(name)).x;
+			//GUILayout.Label(name, GUILayout.Width(temp));
+			GUILayout.Label(new GUIContent(name));
+		}
+		EditorUtility.SetDirty(target);
+	}
+	*/
+}
+#endif
---
+++ Assets/Scripts/GUI/ShadowOption.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+[RequireComponent(typeof(UIPopupList))]
+public class ShadowOption : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	public void stuff(){
+		switch(GetComponent<UIPopupList>().value){
+			case "On":
+			//QualitySettings.sha
+			break;
+			case "Off":
+
+			break;
+		}
+	}
+}
