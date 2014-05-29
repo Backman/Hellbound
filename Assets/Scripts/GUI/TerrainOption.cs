b1f395e326ebe1c725024d1eade3db1cbc814936
Peter <peter.nilsson.1991@hotmail.com>
2014-05-29 15:43:09 +0200

    graphic option pausemenu

--- Assets/Scripts/GUI/TerrainOption.cs
+++ Assets/Scripts/GUI/TerrainOption.cs
@@ -1,26 +1,29 @@
 ﻿using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+
 [RequireComponent(typeof(UIPopupList))]
-public class ShadowOption : MonoBehaviour {
+public class TerrainOption : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-
+	
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
+	
 	}
-
 	public void stuff(){
 		switch(GetComponent<UIPopupList>().value){
-			case "On":
-			QualitySettings.shadowDistance = 10;
+		case "High":
+			QualitySettings.lodBias = 3;
 			break;
-			case "Off":
-			QualitySettings.shadowDistance = 0;
+		case "Medium":
+			QualitySettings.lodBias = 2;
+			break;
+		case "Low":
+			QualitySettings.lodBias = 1;
 			break;
 		}
 	}
