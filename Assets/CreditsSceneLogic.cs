f08851bbd7c82ac766ca49f060019f4f310ed195
Backman <Arrebarree@gmail.com>
2014-05-30 14:18:01 +0200

    Ending stuffs

---
+++ Assets/CreditsSceneLogic.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CreditsSceneLogic : MonoBehaviour {
+
+	public string m_LevelToLoad;
+	public UIScrollView m_CreditsView;
+	public UITweener m_FadeTweener;
+	private UIProgressBar m_CreditsViewScroll;
+
+	public float m_ScrollSpeed = 1.0f;
+	private float m_Yvalue = 1.0f;
+	private bool m_TweenerIsDone = false;
+	void Awake() {
+		if(m_CreditsView != null) {
+			m_CreditsViewScroll = m_CreditsView.verticalScrollBar;
+			m_CreditsViewScroll.value = 0.0f;
+			UICamera.selectedObject = m_CreditsViewScroll.gameObject;
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(InputManager.getButtonDown(InputManager.Button.Pause)) {
+			if(!string.IsNullOrEmpty(m_LevelToLoad)) {
+				if(m_FadeTweener != null) {
+					m_TweenerIsDone = false;
+					m_FadeTweener.PlayReverse();
+					StartCoroutine("startLoadLevel");
+				} else {
+					Application.LoadLevel(m_LevelToLoad);
+				}
+			}
+		}
+
+		if(m_CreditsViewScroll != null) {
+			m_CreditsViewScroll.value += m_ScrollSpeed * Time.deltaTime;
+		}
+	}
+
+	public void tweenerIsDone() {
+		m_TweenerIsDone = true;
+	}
+
+	IEnumerator startLoadLevel() {
+		AsyncOperation ao = Application.LoadLevelAsync(m_LevelToLoad);
+		ao.allowSceneActivation = false;
+
+		while(!m_TweenerIsDone) {
+			yield return null;
+		}
+
+		ao.allowSceneActivation = true;
+	}
+}
